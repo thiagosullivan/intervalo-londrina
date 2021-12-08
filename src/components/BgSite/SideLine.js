f838638d3decdc02dd5d89c2f337103bca63fa1b
thiagosullivan <thiagosullivan12@gmail.com>
2021-12-08 01:14:34 -0300

    Changes for deploy 5

--- src/components/BgSite/SideLine.js
+++ src/components/BgSite/SideLine.js
@@ -5,10 +5,10 @@ function SideLine() {
     <SideLineContainer>
       <svg xmlns="http://www.w3.org/2000/svg" width="2" height="606" viewBox="0 0 2 606">
         <g id="Linhas_laterais" data-name="Linhas laterais" transform="translate(1)">
-          <line id="Linha_24" data-name="Linha 24" y2="432" transform="translate(0 89)" fill="none" stroke="#4d4343" stroke-width="2"/>
-          <line id="Linha_26" data-name="Linha 26" y2="14.5" transform="translate(0 44.5)" fill="none" stroke="#4d4343" stroke-width="2"/>
-          <line id="Linha_28" data-name="Linha 28" y2="14.5" fill="none" stroke="#4d4343" stroke-width="2"/>
-          <line id="Linha_27" data-name="Linha 27" y2="55" transform="translate(0 551)" fill="none" stroke="#4d4343" stroke-width="2"/>
+          <line id="Linha_24" data-name="Linha 24" y2="432" transform="translate(0 89)" fill="none" stroke="#4d4343" strokeWidth="2"/>
+          <line id="Linha_26" data-name="Linha 26" y2="14.5" transform="translate(0 44.5)" fill="none" stroke="#4d4343" strokeWidth="2"/>
+          <line id="Linha_28" data-name="Linha 28" y2="14.5" fill="none" stroke="#4d4343" strokeWidth="2"/>
+          <line id="Linha_27" data-name="Linha 27" y2="55" transform="translate(0 551)" fill="none" stroke="#4d4343" strokeWidth="2"/>
         </g>
       </svg>
     </SideLineContainer>
